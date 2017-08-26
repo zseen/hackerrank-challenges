4f45f20aeb0bd2a76b5056e5ff8424f5cff7ca1a
zseen <43151120+zseen@users.noreply.github.com>
2017-08-26 17:27:28 +0100

    Add exercise DrawingBook - WIP

---
+++ DrawingBook.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+def solve(n, p):
+    # Complete this function
+
+numberOfPages = int(input().strip())
+pageNeeded = int(input().strip())
+result = solve(numberOfPages, pageNeeded)
+print(result)
