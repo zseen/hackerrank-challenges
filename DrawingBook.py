05b66f6a1cde96f2fbdec51d2240d5a6f660b8b8
zseen <43151120+zseen@users.noreply.github.com>
2017-08-28 11:45:35 +0100

    Add prototype Book class

--- DrawingBook.py
+++ DrawingBook.py
@@ -4,7 +4,6 @@ import sys
 
 
 class BookPage(object):
-
     def __init__(self, left=None, right=None):
         self.__left = left
         self.__right = right
@@ -14,56 +13,63 @@ class BookPage(object):
             return True
         return False
 
-def generateBookPages(howManyPages):
-    pageList = []
-    for item in range(0, howManyPages+1):
-        pageList.append(item)
-    return pageList
 
+class Book:
+    def __init__(self, numberOfPages):
+        self.numberOfPages = numberOfPages
 
-def getBookPagesList(fromEnd, numberOfPages):
-    page = generateBookPages(numberOfPages)
+    def generateBookPages(self, howManyPages):
+        pageList = []
+        for item in range(0, howManyPages+1):
+            pageList.append(item)
+        return pageList
 
-    if len(page) % 2 != 0:
-        page.append(0)
+    def getBookPagesList(self, fromEnd, numberOfPages):
+        page = self.generateBookPages(numberOfPages)
 
-    if fromEnd is True:
-        page = page[::-1]
+        if len(page) % 2 != 0:
+            page.append(0)
 
-    book = []
-    for i in range(0, len(page), 2):
-        b = BookPage(page[i], page[i+1])
-        book.append(b)
+        if fromEnd is True:
+            page = page[::-1]
 
-    return book
+        book = []
+        for i in range(0, len(page), 2):
+            b = BookPage(page[i], page[i+1])
+            book.append(b)
 
+        return book
 
-def turnPage(bookPagesList, pageNeeded):
-    count = 0
-    for bookPage in bookPagesList:
-        if bookPage.matches(pageNeeded):
-            return count
-        count += 1
+    def turnPage(self, bookPagesList, pageNeeded):
+        count = 0
+        for bookPage in bookPagesList:
+            if bookPage.matches(pageNeeded):
+                return count
+            count += 1
 
+    def solve(self, pageNeeded, numberOfPages):
+        bookPagesFromBeginning = self.getBookPagesList(False, numberOfPages)
+        turnBegin = self.turnPage(bookPagesFromBeginning, pageNeeded)
 
-def solve(pageNeeded, numberOfPages):
-    bookPagesFromBeginning = getBookPagesList(False, numberOfPages)
-    turnBegin = turnPage(bookPagesFromBeginning, pageNeeded)
+        bookPagesFromEnd = self.getBookPagesList(True, numberOfPages)
+        turnEnd = self.turnPage(bookPagesFromEnd, pageNeeded)
 
-    bookPagesFromEnd = getBookPagesList(True, numberOfPages)
-    turnEnd = turnPage(bookPagesFromEnd, pageNeeded)
+        if turnBegin > turnEnd:
+            return turnEnd
+        else:
+            return turnBegin
 
-    if turnBegin > turnEnd:
-        return turnEnd
-    else:
-        return turnBegin
+    def find(self, pageNeeded):
+        return self.solve(pageNeeded, self.numberOfPages)
 
 
 def main():
     sys.stdin = open('drawingBook_input.txt')
     numberOfPages = int(input().strip())
     pageNeeded = int(input().strip())
-    result = solve(pageNeeded, numberOfPages)
+
+    book = Book(numberOfPages)
+    result = book.find(pageNeeded)
     print(result)
 
 if __name__ == "__main__":
