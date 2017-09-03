c2756ff28d2eee6968c5487b7eaf83930a0da141
zseen <43151120+zseen@users.noreply.github.com>
2017-09-03 15:11:03 +0100

    Refactor CatsAndAMouse - uses abs(), function catBWins has been removed

--- CatsAndAMouse.py
+++ CatsAndAMouse.py
@@ -4,58 +4,38 @@ import sys
 
 
 def catAWins(catA, catB, mouse):
-    if 0 < mouse - catA < mouse - catB:
+    if abs(mouse - catA) < abs(mouse - catB):
         return True
-    elif 0 < catA - mouse < catB - mouse:
-        return True
-    elif 0 < mouse - catA < catB - mouse:
-        return True
-    elif 0 < catA - mouse < mouse - catB:
-        return True
-
     return False
 
 
 def mouseWins(catA, catB, mouse):
-    if mouse - catA == catB - mouse:
-        return True
-    elif mouse - catA == mouse - catB:
-        return True
-    elif catA - mouse == catB - mouse:
-        return True
-    elif catA - mouse == mouse - catB:
-        return True
-    return False
-
-
-def catBWins(catA, catB, mouse):
-    if catAWins(catA, catB, mouse) is False and mouseWins(catA, catB, mouse) is False:
+    if abs(mouse - catA) == abs(catB - mouse):
         return True
     return False
 
 
 def whoWins(catA, catB, mouse):
     catAWinner = catAWins(catA, catB, mouse)
-    catBwinner = catBWins(catA, catB, mouse)
     mouseWinner = mouseWins(catA, catB, mouse)
 
     if catAWinner is True:
         return "Cat A"
-    elif catBwinner is True:
-        return "Cat B"
-    else:
+    elif mouseWinner is True:
         return "Mouse C"
+    else:
+        return "Cat B"
 
 
 def main():
-     sys.stdin = open('catsAndAMouse_input.txt')
-     q = int(input().strip())
-     for a0 in range(q):
-         x, y, z = input().strip().split(' ')
-         x, y, z = [int(x), int(y), int(z)]
+    sys.stdin = open('catsAndAMouse_input.txt')
+    q = int(input().strip())
+    for a0 in range(q):
+        x, y, z = input().strip().split(' ')
+        x, y, z = [int(x), int(y), int(z)]
 
-         winner = whoWins(x, y, z)
-         print(winner)
+        winner = whoWins(x, y, z)
+        print(winner)
 
 if __name__ == "__main__":
     main()
