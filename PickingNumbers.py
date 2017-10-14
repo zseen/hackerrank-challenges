087550ad614f4365a06893fedfd1db9481cc2abe
zseen <43151120+zseen@users.noreply.github.com>
2017-10-14 20:32:32 +0100

    Refactor - delete old solution
    - The old solution did not work anyway

--- PickingNumbers.py
+++ PickingNumbers.py
@@ -6,34 +6,13 @@ import sys
 
 
 def differenceIsLessThanOne(numbersList):
-    #counter = Counter(numbersList).most_common()
-    #print(counter)
-    # biggerNum = 0
-    # howManyBigger = 0
-    # smallerNum = 0
-    # howManySmaller = 0
-    # middlenum = 0
-    # howManyMiddle = 0
-    # for item in counter:
-    #     middleNum = counter[0][0]
-    #     howManyMiddle = counter[0][1]
-    #     if item[0] - 1 == middleNum:
-    #         biggerNum = item[0]
-    #         howManyBigger = item[1]
-    #     elif item[0] + 1 == middleNum:
-    #         smallerNum = item[0]
-    #         howManySmaller = item[1]
-    # if howManyBigger > howManySmaller:
-    #     return howManyMiddle + howManyBigger
-    # return howManySmaller + howManyMiddle
-
     maximum = 0
     for item in numbersList:
-        howManyBig = numbersList.count(item)
-        howManySmall = numbersList.count(item - 1)
-        howManyBig = howManyBig + howManySmall
-        if howManyBig > maximum:
-            maximum = howManyBig
+        numBig = numbersList.count(item)
+        numSmall = numbersList.count(item - 1)
+        total = numBig + numSmall
+        if total > maximum:
+            maximum = total
     return maximum
 
 
