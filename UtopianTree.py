b984b7dfb02384658a3a9c1235c2a0a6edb69c7b
zseen <43151120+zseen@users.noreply.github.com>
2017-10-22 16:43:35 +0100

    Add this exercise - refactored

--- UtopianTree.py
+++ UtopianTree.py
@@ -7,18 +7,16 @@ def growthHalfCycle(n):
     currHeight = growthFullCycle(n)
     return currHeight * 2
 
+
 def growthFullCycle(n):
     currentHeight = 1
-    for i in range(1,(n//2)+1):
-        currentHeight = currentHeight * 2
+    for _ in range(0, n//2):
+        currentHeight *= 2
         currentHeight += 1
     return currentHeight
 
 
 def getHeight(n):
-
-
-
     treeHeights = []
 
     if n % 2 == 0:
@@ -31,25 +29,13 @@ def getHeight(n):
     return treeHeights
 
 
-
-            #if n == 4:
-            #1*2 , 1*2+1
-            #3*2 , 3*2+1
-            #if n == 5:
-            #1*2 , 1*2+1
-            #3*2 , 3*2+1
-            #(3*2+1)
-
 def main():
     sys.stdin = open('utopianTree_input.txt')
     t = int(input().strip())
 
     for a0 in range(t):
         n = int(input().strip())
-        #print(n)
-
         height = getHeight(n)
-
         for item in height:
             print(item)
 
