39a5b42f0e0495edc68b0049045a5d8a0304211f
zseen <43151120+zseen@users.noreply.github.com>
2017-11-19 11:12:40 +0000

    Refactor - add main

--- SaveThePrisoner.py
+++ SaveThePrisoner.py
@@ -13,10 +13,14 @@ def saveThePrisoner(numberOfPrisoners, numberOfSweets, prisonerID):
     return warned if warned != 0 else numberOfPrisoners
 
 
-sys.stdin = open('saveThePrisoner_input.txt')
-t = int(input().strip())
-for a0 in range(t):
-    numberOfPrisoners, numberOfSweets, prisonerID = input().strip().split(' ')
-    numberOfPrisoners, numberOfSweets, prisonerID = [int(numberOfPrisoners), int(numberOfSweets), int(prisonerID)]
-    result = saveThePrisoner(numberOfPrisoners, numberOfSweets, prisonerID)
-    print(result)
+def main():
+    sys.stdin = open('saveThePrisoner_input.txt')
+    t = int(input().strip())
+    for a0 in range(t):
+        numberOfPrisoners, numberOfSweets, prisonerID = input().strip().split(' ')
+        numberOfPrisoners, numberOfSweets, prisonerID = [int(numberOfPrisoners), int(numberOfSweets), int(prisonerID)]
+        result = saveThePrisoner(numberOfPrisoners, numberOfSweets, prisonerID)
+        print(result)
+
+if __name__ == "__main__":
+    main()
