18bb0fe4f42f20c4e66799c5908479f96768135f
zseen <43151120+zseen@users.noreply.github.com>
2017-11-19 21:20:25 +0000

    Add this exercise - refactored

--- SuperReducedString.py
+++ SuperReducedString.py
@@ -2,16 +2,31 @@
 
 import sys
 
+
 def super_reduced_string(s):
     charList = list(s)
-    for i in range(0, len(charList) - 1):
-        if charList[i] == charList[i + 1]:
-            charList.pop(i)
-            charList.pop(i)
-    return s
+    chIndex = 0
+    while chIndex < len(charList) - 1:
+        if charList[chIndex] == charList[chIndex + 1]:
+            del charList[chIndex]
+            del charList[chIndex]
+            chIndex = 0
+            if len(charList) == 0:
+                return False
+        else:
+            chIndex += 1
+    return ''.join(charList)
 
 
-sys.stdin = open('superReducedString_input.txt')
-string = input().strip()
-result = super_reduced_string(string)
-print(result)
+def main():
+    sys.stdin = open('superReducedString_input.txt')
+    string = input().strip()
+    result = super_reduced_string(string)
+    if result is False:
+        print("Empty String")
+    else:
+        print(result)
+
+if __name__ == "__main__":
+    main()
+
