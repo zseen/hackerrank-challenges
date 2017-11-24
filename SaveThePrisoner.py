6b4eeaf6be001e08ccc28bee72e4b7eacd086821
zseen <43151120+zseen@users.noreply.github.com>
2017-11-24 17:23:44 +0000

    Refactor - use index, remove if-else

--- SaveThePrisoner.py
+++ SaveThePrisoner.py
@@ -3,15 +3,13 @@
 import sys
 
 
-def saveThePrisoner(numberOfPrisoners, numberOfSweets, prisonerID):
+def getPrisonerIDToWarn(numberOfPrisoners, numberOfSweets, prisonerID):
     remainingSweets = numberOfSweets % numberOfPrisoners
 
-    if prisonerID + remainingSweets > numberOfPrisoners:
-        warned = remainingSweets - (numberOfPrisoners - prisonerID) - 1
-    else:
-        warned = prisonerID + remainingSweets - 1
-    return warned if warned != 0 else numberOfPrisoners
+    firstIndexToGet = prisonerID - 1
+    lastIndexToGet = (firstIndexToGet + remainingSweets - 1) % numberOfPrisoners
 
+    return lastIndexToGet + 1
 
 def main():
     sys.stdin = open('saveThePrisoner_input.txt')
@@ -19,7 +17,7 @@ def main():
     for a0 in range(t):
         numberOfPrisoners, numberOfSweets, prisonerID = input().strip().split(' ')
         numberOfPrisoners, numberOfSweets, prisonerID = [int(numberOfPrisoners), int(numberOfSweets), int(prisonerID)]
-        result = saveThePrisoner(numberOfPrisoners, numberOfSweets, prisonerID)
+        result = getPrisonerIDToWarn(numberOfPrisoners, numberOfSweets, prisonerID)
         print(result)
 
 if __name__ == "__main__":
