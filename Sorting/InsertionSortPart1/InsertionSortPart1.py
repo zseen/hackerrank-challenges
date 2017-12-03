8451af3c3a1d6166687e833c852ca8b2e6d0705d
zseen <43151120+zseen@users.noreply.github.com>
2017-12-03 14:51:43 +0000

    Add this exercise - refactored

--- Sorting/InsertionSortPart1/InsertionSortPart1.py
+++ Sorting/InsertionSortPart1/InsertionSortPart1.py
@@ -1,16 +1,7 @@
 import sys
-sys.stdin = open('InsertionSortPart1_input.txt')
-
-#14
-#1 3 5 9 13 22 27 35 46 51 55 83 87 23
 
 
-n = int(input().strip())
-listToSort = list(map(int, input().strip().split(' ')))
-
-
-
-def sortList():
+def sortList(listToSort):
     itemToInsert = listToSort[len(listToSort)-1]
     listToSort.pop()
     length = len(listToSort) - 1
@@ -18,34 +9,29 @@ def sortList():
     listToSort.append(listToSort[len(listToSort) - 1])
     for item in listToSort:
         originalListToSort.append(item)
-
     print(' '.join(map(str, listToSort)))
-
-
+    
     for index in range(length):
-        if listToSort[length - (index )] > itemToInsert and length - index != 1:
-            listToSort[length - index ] = listToSort[length - index - 1]
-            x = listToSort
+        if listToSort[length - (index + 1)] > itemToInsert:
+            listToSort[length - index] = listToSort[length - index - 1]
             print(' '.join(map(str, listToSort)))
             listToSort[length - index - 1] = originalListToSort[length - index - 1]
-
-
+            if length - (index + 1) == 0 and listToSort[length - (index + 1)] > itemToInsert:
+                listToSort[0] = itemToInsert
+                return listToSort
 
         else:
-            listToSort[length - index +1] = itemToInsert
-            #listToSort.insert(length - index , itemToInsert)
-            #del listToSort[length-index + 1]
-            #listToSort.pop()
-            print(' '.join(map(str, listToSort)))
-            break
+            listToSort[length - index] = itemToInsert
+            return listToSort
 
 
+def main():
+    sys.stdin = open('InsertionSortPart1_input.txt')
+    n = int(input().strip())
+    listToSort = list(map(int, input().strip().split(' ')))
+    sortedList = sortList(listToSort)
+    print(' '.join(map(str, sortedList)))
 
 
-
-
-
-
-
-
-sortList()
+if __name__ == "__main__":
+    main()
