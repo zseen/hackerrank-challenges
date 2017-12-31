c8da303f9ecedb688d154d21640470b81f486001
zseen <43151120+zseen@users.noreply.github.com>
2017-12-31 21:27:59 +0000

    Refactor - have a main()

--- Sorting/InsertionSortPart2.py
+++ Sorting/InsertionSortPart2.py
@@ -1,10 +1,10 @@
 import sys
 
+
 def sortList(listToBeSorted):
     index = 0
 
     while index < len(listToBeSorted) - 1:
-
 
         if listToBeSorted[index] > listToBeSorted[index + 1]:
             print(' '.join(map(str, listToBeSorted)))
@@ -13,14 +13,17 @@ def sortList(listToBeSorted):
             index -= 1
         else:
             index += 1
-            #print(' '.join(map(str, listToBeSorted)))
+
     return listToBeSorted
 
 
+def main():
+    sys.stdin = open('InsertionSortPart2_input.txt')
+    n = int(input().strip())
+    listToBeSorted = list(map(int, input().strip().split(' ')))
+    listSorted = sortList(listToBeSorted)
+    print(' '.join(map(str, listSorted)))
 
-sys.stdin = open('InsertionSortPart2_input.txt')
-n = int(input().strip())
-listToSort = list(map(int, input().strip().split(' ')))
 
-sortedList = sortList(listToSort)
-print(' '.join(map(str, sortedList)))
+if __name__ == "__main__":
+    main()
