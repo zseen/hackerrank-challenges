6d1eb18a29adc5a793ce5231141065af6de3f6f4
zseen <43151120+zseen@users.noreply.github.com>
2018-02-17 21:16:11 +0000

    Add this exercise - use merge sort, work in progress

--- Sorting/InsertionSortAdvancedAnalysis/InsertionSortAdvancedAnalysis.py
+++ Sorting/InsertionSortAdvancedAnalysis/InsertionSortAdvancedAnalysis.py
@@ -2,8 +2,40 @@
 
 import sys
 
-def insertionSort(listToBeSorted):
+def mergeSort(listToBeSorted):
     counter = 0
+    if len(listToBeSorted) > 1:
+        mid = len(listToBeSorted) // 2
+        leftHalf = listToBeSorted[:mid]
+        rightHalf = listToBeSorted[mid:]
+
+        mergeSort(leftHalf)
+        mergeSort(rightHalf)
+
+        leftListIndex = 0
+        rightListIndex = 0
+        originalListIndex = 0
+        while leftListIndex < len(leftHalf) and rightListIndex < len(rightHalf):
+            if leftHalf[leftListIndex] < rightHalf[rightListIndex]:
+                listToBeSorted[originalListIndex] = leftHalf[leftListIndex]
+                leftListIndex += 1
+                counter += 1
+            else:
+                listToBeSorted[originalListIndex] = rightHalf[rightListIndex]
+                rightListIndex += 1
+                counter += 1
+            originalListIndex += 1
+
+        while leftListIndex < len(leftHalf):
+            listToBeSorted[originalListIndex] = leftHalf[leftListIndex]
+            leftListIndex += 1
+            originalListIndex += 1
+
+        while rightListIndex < len(rightHalf):
+            listToBeSorted[originalListIndex] = rightHalf[rightListIndex]
+            rightListIndex += 1
+            originalListIndex += 1
+    return listToBeSorted
 
 
 
@@ -14,5 +46,5 @@ if __name__ == "__main__":
     for a0 in range(t):
         n = int(input().strip())
         numbersList = list(map(int, input().strip().split(' ')))
-        result = insertionSort(numbersList)
+        result = mergeSort(numbersList)
         print(result)
