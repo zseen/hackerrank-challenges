d846056ad6b7308d2cc5d6ad900b496cf9cdb3ba
zseen <43151120+zseen@users.noreply.github.com>
2018-02-25 11:02:32 +0000

    Start this exercise

---
+++ Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+def activityNotifications(expenditure, d):
+    # Complete this function
+
+if __name__ == "__main__":
+    n, d = input().strip().split(' ')
+    n, d = [int(n), int(d)]
+    expenditure = list(map(int, input().strip().split(' ')))
+    result = activityNotifications(expenditure, d)
+    print(result)
