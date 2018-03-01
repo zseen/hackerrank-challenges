bf6066518fd619a2bbccf3d552284639eb7795fc
zseen <43151120+zseen@users.noreply.github.com>
2018-03-01 15:49:15 +0000

    Start this exercise

--- Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
+++ Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
@@ -1,13 +1,31 @@
 #!/bin/python3
 
 import sys
+from Library.Sorting import CountingSort as cs
 
-def activityNotifications(expenditure, d):
-    # Complete this function
+def activityNotifications(moneySpentDaily, daysPrior):
+    notificationsCount = 0
+    daysPriorIndex = daysPrior - 1
+    initialMedianSpent = 0
+    initialMedianSpentList = []
+    for i in range(0, daysPrior):
+        initialMedianSpentList.append(moneySpentDaily[i])
+    sortedInitialSpent = cs.sortListOfNumbers(initialMedianSpentList)
+    initialMedianSpent = sortedInitialSpent[len(sortedInitialSpent) // 2]
+
+
+
+    for index in range(daysPriorIndex, len(moneySpentDaily) - daysPriorIndex):
+        moneySpentForDaysPrior = []
+        for i in range(0, daysPrior):
+            moneySpentForDaysPrior.append(moneySpentDaily[index + i])
+        return moneySpentForDaysPrior
+
 
 if __name__ == "__main__":
-    n, d = input().strip().split(' ')
-    n, d = [int(n), int(d)]
-    expenditure = list(map(int, input().strip().split(' ')))
-    result = activityNotifications(expenditure, d)
+    sys.stdin = open('FraudulentActivityNotifications_input.txt')
+    daysNumData, daysPrior = input().strip().split(' ')
+    daysNumData, daysPrior = [int(daysNumData), int(daysPrior)]
+    moneySpentDaily = list(map(int, input().strip().split(' ')))
+    result = activityNotifications(moneySpentDaily, daysPrior)
     print(result)
