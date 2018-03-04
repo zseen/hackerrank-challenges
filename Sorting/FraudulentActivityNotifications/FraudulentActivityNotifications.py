62b978e25401107557ef2f225ff90c2d3dced511
zseen <43151120+zseen@users.noreply.github.com>
2018-03-04 17:34:14 +0000

    Work on short input, runtime error on long ones

--- Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
+++ Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
@@ -5,35 +5,21 @@ from Library.Sorting import CountingSort as cs
 
 
 def activityNotifications(moneySpentDaily, daysPrior):
-    notificationsCount = 0
-    daysPriorIndex = daysPrior - 1
-    initialMedianSpent = 0
-    initialMedianSpentList = []
-    for i in range(0, daysPrior):
-        initialMedianSpentList.append(moneySpentDaily[i])
-
-
-
     moneySpentForDaysPrior = []
     for index in range(0, len(moneySpentDaily) - daysPrior):
-
         for i in range(0, daysPrior):
             moneySpentForDaysPrior.append(moneySpentDaily[index + i])
-    #print(moneySpentForDaysPrior)
+    print(moneySpentForDaysPrior)
 
     subListMoneySpentPrior = [moneySpentForDaysPrior[n:n + 5] for n in range(0, len(moneySpentForDaysPrior), 5)]
-    print(subListMoneySpentPrior)
-
-    sortedInitialSpent = cs.sortListOfNumbers(subListMoneySpentPrior[1])
-    initialMedianSpent = sortedInitialSpent[len(sortedInitialSpent) // 2]
-    print(initialMedianSpent)
+    #print(subListMoneySpentPrior)
 
     counter = 0
 
     for index in range(1, len(subListMoneySpentPrior)):
-        sortedInitialSpent = cs.sortListOfNumbers(subListMoneySpentPrior[index])
-        initialMedianSpent = sortedInitialSpent[len(sortedInitialSpent) // 2]
-        if initialMedianSpent < moneySpentForDaysPrior[index * 5]:
+        sortedDailySpent = cs.sortListOfNumbers(subListMoneySpentPrior[index])
+        medianSpent = sortedDailySpent[len(sortedDailySpent) // 2]
+        if medianSpent < moneySpentDaily[index + 5]:
             counter += 1
     return counter
 
