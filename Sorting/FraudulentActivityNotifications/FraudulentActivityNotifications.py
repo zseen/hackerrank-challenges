27feef00c6c2b3c3b5bd929a80fa7558fcab7a7b
zseen <43151120+zseen@users.noreply.github.com>
2018-04-15 21:31:50 +0100

    Attempt to make this exercise work - unittest not working, median is still wrong

--- Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
+++ Sorting/FraudulentActivityNotifications/FraudulentActivityNotifications.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 import sys
+import unittest
 
 
 def getNumCounter(moneySpentDaily, daysPrior):
@@ -19,8 +20,8 @@ def getNumCounter(moneySpentDaily, daysPrior):
     return counter
 
 
-def getMedian(moneySpentDaily, daysPrior):
-    counter = getNumCounter(moneySpentDaily, daysPrior)
+def getMedian(daysPrior, COUNTER):
+    counter = COUNTER
 
     if daysPrior % 2 != 0:
         medianItem = (daysPrior // 2) + 1
@@ -43,14 +44,14 @@ def getMedian(moneySpentDaily, daysPrior):
                 return median
 
 
-def countNotifications(moneySpentDaily, daysPrior):
+def countNotifications(COUNTER, moneySpentDaily, daysPrior):
     notificationCounter = 0
 
     for index in range(0, len(moneySpentDaily) - daysPrior):
         moneySpentForDaysPrior = []
         for i in range(0, daysPrior):
             moneySpentForDaysPrior.append(moneySpentDaily[index + i])
-        median = getMedian(moneySpentForDaysPrior, daysPrior)
+        median = getMedian(daysPrior, COUNTER)
         if moneySpentDaily[index + daysPrior] >= median * 2:
             notificationCounter += 1
     return notificationCounter
@@ -61,10 +62,37 @@ def main():
     daysNumData, daysPrior = input().strip().split(' ')
     daysNumData, daysPrior = [int(daysNumData), int(daysPrior)]
     moneySpentDaily = list(map(int, input().strip().split(' ')))
+    COUNTER = getNumCounter(moneySpentDaily, daysPrior)
 
-    notifications = countNotifications(moneySpentDaily, daysPrior)
+    notifications = countNotifications(COUNTER, moneySpentDaily, daysPrior)
     print(notifications)
+    #should be 633
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    unittest.main()
+
+
+class TestNotificationCount(unittest.TestCase):
+
+    def test_5daysPrior_2notifications(self, COUNTER):
+        daysPrior = 5
+        moneySpentDaily = [2, 3, 4, 2, 3, 6, 8, 4, 5]
+        notifications = countNotifications(COUNTER, moneySpentDaily, daysPrior)
+        self.assertTrue(notifications == 2)
+
+    def test_4daysPrior_0notifications(self, COUNTER):
+        daysPrior = 4
+        moneySpentDaily = [1, 2, 3, 4, 4]
+        notifications = countNotifications(COUNTER, moneySpentDaily, daysPrior)
+        self.assertTrue(notifications == 0)
+
+    def test_3daysPrior_1notification(self, COUNTER):
+        daysPrior = 3
+        moneySpentDaily = [1, 2, 3, 4, 5]
+        notifications = countNotifications(COUNTER, moneySpentDaily, daysPrior)
+        self.assertTrue(notifications == 1)
+
+    def t4(self):
+        self.assertTrue(0 == 0)
