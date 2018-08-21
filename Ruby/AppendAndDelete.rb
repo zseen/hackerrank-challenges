c40ec98cf35f05fb6f51d3c842f663e4b91c66ad
zseen <43151120+zseen@users.noreply.github.com>
2018-08-21 11:45:24 +0100

    Refactor - add new method for the return value of appendAndDelete, fix indentation

--- Ruby/AppendAndDelete.rb
+++ Ruby/AppendAndDelete.rb
@@ -6,82 +6,77 @@ require 'stringio'
 
 
 def appendAndDelete(string1, string2, steps)
-    chars_to_chop = (string1.length - string2.length).abs
+    chars_to_trim = (string1.length - string2.length).abs
     
-    remaining_steps = steps - chars_to_chop
+    remaining_steps = steps - chars_to_trim
   
-    same_length_strings_list = chopSameLength(string1, string2)  
+    same_length_strings_list = trimSameLength(string1, string2)  
     shortened_str1 = same_length_strings_list[0]
     shortened_str2 = same_length_strings_list[1]
- 
-    if remaining_steps >= 2 * shortened_str1.length      
-      return true
-    end
     
-    check = checkTwoStringsSame(shortened_str1, shortened_str2)
-  
-    if check == true 
-      if remaining_steps % 2 == 0
-        return true
-      else
-        return false
-      end     
-        
-    else
-      first_different_index = compareStringsFindFirstDifference(shortened_str1, shortened_str2)
-      num_chars_to_change = shortened_str1.length - first_different_index
+    result = returnValueAppendAndDelete(shortened_str1, shortened_str2, remaining_steps)
+    return result  
+end
 
-        if (remaining_steps - num_chars_to_change *2) % 2 == 0 and remaining_steps - num_chars_to_change * 2 >= 0
-        return true
-        end 
-      end
-    return false  
-    end
-  
-  def compareStringsFindFirstDifference(s1, s2)
-      for index in (0...s1.length)
-        if s1[index] != s2[index]
-          return index 
-        end
-      end  
-    end
-   
-  def checkTwoStringsSame(string1, string2)
-    if string1 == string2
-      return true
-    else  
-      return false
-    end
-    end  
-  
-  def chopSameLength(string1, string2)
-    if string1.length >= string2.length
-      shortened_str1 = string1.byteslice(0, string2.length)
-      return shortened_str1, string2
-    else
-      shortened_str2 = string2.byteslice(0, string1.length)
-      return string1, shortened_str2 
-      end
+
+def returnValueAppendAndDelete(s1, s2, moves)
+  if moves >= 2 * s1.length      
+    return true
   end
-
-
-  fptr = File.open(ENV['OUTPUT_PATH'], 'w')
-
-  s = gets.to_s.rstrip
-  
-  t = gets.to_s.rstrip
-  
-  k = gets.to_i
-  
-  result = appendAndDelete s, t, k
-  if result == true
-      result = "Yes"
+    
+  if s1 == s2
+    return moves % 2 == 0    
+        
   else
-      result = "No"
-  end    
+    first_different_index = compareStringsFindFirstDifference(s1, s2)
+    num_chars_to_change = s1.length - first_different_index
+    necessary_steps = moves - num_chars_to_change *2
+
+    if necessary_steps % 2 == 0 and necessary_steps >= 0
+      return true
+    end 
+  end
+  return false  
+end  
+
   
-  fptr.write result
-  fptr.write "\n"
+def compareStringsFindFirstDifference(s1, s2)
+  for index in (0...s1.length)
+    if s1[index] != s2[index]
+      return index 
+    end
+  end  
+end
+   
   
-  fptr.close()
-     
+def trimSameLength(string1, string2)
+  if string1.length >= string2.length
+    shortened_str1 = string1.byteslice(0, string2.length)
+    return shortened_str1, string2
+  else
+    shortened_str2 = string2.byteslice(0, string1.length)
+    return string1, shortened_str2 
+  end
+end
+
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+s = gets.to_s.rstrip
+
+t = gets.to_s.rstrip
+
+k = gets.to_i
+
+result = appendAndDelete s, t, k
+if result == true
+    result = "Yes"
+else
+    result = "No"
+end    
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
+    
