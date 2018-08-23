8910172ee0377911ee6302f9354278459854397e
zseen <43151120+zseen@users.noreply.github.com>
2018-08-23 17:27:45 +0100

    Refactor - make function compareStringsFindFirstDifferentIndex() able to handle subtle cases as well

--- Ruby/AppendAndDelete.rb
+++ Ruby/AppendAndDelete.rb
@@ -1,56 +1,49 @@
-#!/bin/ruby
+
 
 require 'json'
 require 'stringio'
 
 
 def appendAndDelete(s1, s2, moves)
-  if moves >= 2 * [s1.length, s2.length].min + (s1.length - s2.length).abs     
+  if s1.length + s2.length - moves < 0
     return true
-
-  elsif s1 == s2 
-    return moves % 2 == 0  
-        
-  else
-    first_different_index = compareStringsFindFirstDifference(s1, s2)
-
-    necessary_steps = moves - (s1.length - first_different_index) - (s2.length - first_different_index)
-
-    if necessary_steps % 2 == 0 and necessary_steps >= 0
-      return true
-    end 
   end
+        
+  first_different_index = compareStringsFindFirstDifferentIndex(s1, s2)
+  remaining_free_steps = moves - (s1.length - first_different_index) - (s2.length - first_different_index)
+  if remaining_free_steps % 2 == 0 and remaining_free_steps >= 0
+    return true
+  end 
   return false  
 end  
 
   
-def compareStringsFindFirstDifference(s1, s2)
-  for index in (0...[s1.length, s2.length].max)
+def compareStringsFindFirstDifferentIndex(s1, s2)
+  for index in (0...[s1.length, s2.length].min)
     if s1[index] != s2[index]
-      return index 
+      return index
     end
   end
+  return [s1.length, s2.length].min + 1
 end
 
 
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
-  fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+s = gets.to_s.rstrip
 
-  s = gets.to_s.rstrip
-  
-  t = gets.to_s.rstrip
-  
-  k = gets.to_i
-  
-  result = appendAndDelete s, t, k
-  if result == true
-      result = "Yes"
-  else
-      result = "No"
-  end    
-  
-  fptr.write result
-  fptr.write "\n"
-  
-  fptr.close()
-     
+t = gets.to_s.rstrip
+
+k = gets.to_i
+
+result = appendAndDelete s, t, k
+if result == true
+    result = "Yes"
+else
+    result = "No"
+end    
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
