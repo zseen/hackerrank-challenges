1aa691a27b9dca45c7499e3dc1090930df2880f6
zseen <43151120+zseen@users.noreply.github.com>
2018-08-25 22:00:55 +0100

    Refactor - add one more false test case, change switch between test mode and normal mode

--- Ruby/AppendAndDelete.rb
+++ Ruby/AppendAndDelete.rb
@@ -3,7 +3,7 @@ require "test/unit"
 require 'json'
 require 'stringio'
 
-IS_UNIT_TEST = true
+TEST_MODE = "OFF"
 
 def appendAndDelete(s1, s2, moves)
   if moves > s1.length + s2.length
@@ -28,7 +28,7 @@ def compareStringsFindFirstDifferentIndex(s1, s2)
   return [s1.length, s2.length].min + 1
 end
 
-if IS_UNIT_TEST == false
+if TEST_MODE == "OFF"
   fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
   s = gets.to_s.rstrip
@@ -48,28 +48,31 @@ if IS_UNIT_TEST == false
   fptr.write "\n"
 
   fptr.close()
+end
 
 
-else
-  class TestAppendAndDelete < Test::Unit::TestCase
+class TestAppendAndDelete < Test::Unit::TestCase
 
-    def test_appendAndDelete_differentLengthStrings_exactNumSteps_notPrefix_true
-        assert_true(appendAndDelete('hackerhappy', 'hackerrank', 9))   
-    end
-    def test_appendAndDelete_sameStrings_emptyDeleteNeeded_true
-        assert_true(appendAndDelete("aba", "aba", 7))
-    end
-    def test_appendAndDelete_differentLengthStringss_Prefix_false
-        assert_false(appendAndDelete("abcd", "abcdert", 10))
-    end
-    def test_appendAndDelete_differentLengthStrings_Prefix_evenSteps_true
-        assert_true(appendAndDelete("zzzzz", "zzzzzzz", 4))
-    end
-    def test_appendAndDelete_differentLengthStrings_firstStringLonger_emptyDeleteNeeded_true
-        assert_true(appendAndDelete("aaa", "a", 5))
-    end
-    def test_appendAndDelete_sameLengthStrings_allCharsDifferent_moreStepsThanSumStringsLength_true
-        assert_true(appendAndDelete("abcdef", "fedcba", 15))
-    end                    
+  def test_appendAndDelete_differentLengthStrings_exactNumSteps_notPrefix_true
+      assert_true(appendAndDelete('hackerhappy', 'hackerrank', 9))   
   end
-end        
+  def test_appendAndDelete_sameStrings_emptyDeleteNeeded_true
+      assert_true(appendAndDelete("aba", "aba", 7))
+  end
+  def test_appendAndDelete_differentLengthStringss_prefix_false
+      assert_false(appendAndDelete("abcd", "abcdert", 10))
+  end
+  def test_appendAndDelete_differentLengthStrings_prefix_evenSteps_true
+      assert_true(appendAndDelete("zzzzz", "zzzzzzz", 4))
+  end
+  def test_appendAndDelete_differentLengthStrings_firstStringLonger_emptyDeleteNeeded_true
+      assert_true(appendAndDelete("aaa", "a", 5))
+  end
+  def test_appendAndDelete_sameLengthStrings_allCharsDifferent_moreStepsThanSumStringsLength_true
+      assert_true(appendAndDelete("abcdef", "fedcba", 15))
+  end
+  def test_appendAndDelete_differentLengthStrings_oneCharLengthDifference_prefix_evenSteps_false
+      assert_false(appendAndDelete("y", "yu", 2))
+  end                        
+end
+      
