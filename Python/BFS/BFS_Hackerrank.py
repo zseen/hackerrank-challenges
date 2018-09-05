733304cbacb0df82d3929e9973b58690e0c21f86
zseen <43151120+zseen@users.noreply.github.com>
2018-09-05 16:02:03 +0100

    Add 10 unittests with edge cases - run properly

--- Python/BFS/BFS_Hackerrank.py
+++ Python/BFS/BFS_Hackerrank.py
@@ -74,52 +74,169 @@ class Graph:
             if item.nodeId == startNodeId:
                 continue
             listOfDistances.append(str(item.distance * 6 if item.distance > 0 else item.distance))
+        print(listOfDistances)
         return listOfDistances
 
-if __name__ == '__main__':
 
-    # sys.stdin = open('BFS_Hackerrank_input.txt')
-    #
-    # q = int(input())
-    #
-    # for q_itr in range(q):
-    #     nm = input().split()
-    #     nodesNum = int(nm[0])
-    #     edgesNum = int(nm[1])
-    #     edges = []
-    #     for _ in range(edgesNum):
-    #         edges.append(list(map(int, input().rstrip().split())))
-    #     #print(edges)
-    #     startNode = int(input())
-    #
-    #     graph = Graph()
-    #     for nodeId in range(1, nodesNum + 1):
-    #         graph.addNode(nodeId)
-    #
-    #     for graphEdges in edges:
-    #         graph.addEdge(graphEdges[0], graphEdges[1])
-    #     #print(type(graph.printNodesDistanceOrder(startNode)))
-    #
-    #     for item in graph.printNodesDistanceOrder(startNode):
-    #         print(item, end=" ")
-    #     #print(result)
+def main():
 
-    graph = Graph()
+    sys.stdin = open('BFS_Hackerrank_input.txt')
 
-    graph.addNode("A")
-    graph.addNode("B")
-    graph.addNode("C")
-    graph.addNode("D")
-    graph.addNode("E")
+    q = int(input())
 
-    graph.addEdge("C", "B")
-    graph.addEdge("C", "D")
-    graph.addEdge("D", "A")
+    for q_itr in range(q):
+        nm = input().split()
+        nodesNum = int(nm[0])
+        edgesNum = int(nm[1])
+        edges = []
+        for _ in range(edgesNum):
+            edges.append(list(map(int, input().rstrip().split())))
+        print(edges)
+        startNode = int(input())
+
+        graph = Graph()
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        #print(type(graph.printNodesDistanceOrder(startNode)))
+
+        for item in graph.printNodesDistanceOrder(startNode):
+            print(item, end=" ")
+        #print(result)
+
 
 
     #graph.printNodesDistanceOrder("C")
 
-    for item in graph.printNodesDistanceOrder("C"):
-        print(item, end=" ")
+
+
+class TestPrintNodesDistanceOrder(unittest.TestCase):
+    def test_printNodesDistanceOrder_startNodeIs1_allNodesConnected_1EdgeLevel(self):
+        nodesNum = 3
+        startNode = 1
+        edges = [[1, 2], [1, 3]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '6']))
+
+    def test_printNodesDistanceOrder_startNodeIs1_allNodesConnected_2EdgeLevels(self):
+        nodesNum = 4
+        startNode = 1
+        edges = [[1, 2], [1, 3], [3, 4]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '6', '12']))
+
+    def test_printNodesDistanceOrder_startNodeIs2_allNodesConnected_1EdgeLevel(self):
+        nodesNum = 3
+        startNode = 2
+        edges = [[2, 1], [2, 3]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '6']))
+
+    def test_printNodesDistanceOrder_startNodeIs1_notAllNodesConnected_1EdgeLevel(self):
+        nodesNum = 4
+        startNode = 1
+        edges = [[1, 2], [1, 3]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '6', '-1']))
+
+    def test_printNodesDistanceOrder_startNodeIs3_notAllNodesConnected_2EdgeLevels(self):
+        nodesNum = 5
+        startNode = 3
+        edges = [[3, 2], [3, 4], [4, 1]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['12', '6', '6', '-1']))
+
+    def test_printNodesDistanceOrder_startNodeIs3_allNodesConnected_3EdgeLevels(self):
+        nodesNum = 7
+        startNode = 3
+        edges = [[1, 2], [3, 1], [2, 4], [2, 5], [3, 5], [3, 6], [3, 7]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '12', '18', '6', '6', '6']))
+
+    def test_printNodesDistanceOrder_startNodeIs3_notAllNodesConnected_3EdgeLevels(self):
+        nodesNum = 7
+        startNode = 3
+        edges = [[1, 2], [3, 1], [2, 4], [2, 5], [3, 5], [3, 7]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '12', '18', '6', '-1', '6']))
+
+    def test_printNodesDistanceOrder_startNodeIs2_noNodesConnected_0EdgeLevels(self):
+        nodesNum = 3
+        startNode = 2
+        edges = []
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['-1', '-1']))
+
+    def test_printNodesDistanceOrder_startNodeIs1_allNodesConnected_1EdgeLevel_edgesAddedTwiceOriginalOrder(self):
+        nodesNum = 3
+        startNode = 1
+        edges = [[1, 2], [1, 3], [1, 2], [1, 3]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '6']))
+
+    def test_printNodesDistanceOrder_startNodeIs1_allNodesConnected_1EdgeLevel_edgesAddedTwiceReversedOrder(self):
+        nodesNum = 3
+        startNode = 1
+        edges = [[1, 2], [1, 3], [3, 1]]
+        graph = Graph()
+
+        for nodeId in range(1, nodesNum + 1):
+            graph.addNode(nodeId)
+        for graphEdges in edges:
+            graph.addEdge(graphEdges[0], graphEdges[1])
+        self.assertTrue((graph.printNodesDistanceOrder(startNode) == ['6', '6']))
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
