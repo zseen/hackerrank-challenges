65a0d77f8ffe38e70db11d7d05a07dee2ed3984c
zseen <43151120+zseen@users.noreply.github.com>
2018-09-25 11:53:50 +0100

    Fix blank lines and move DeleteANode to its own folder

--- Python/LinkedList/DeleteANode/DeleteANode.py
+++ Python/LinkedList/DeleteANode/DeleteANode.py
@@ -6,10 +6,12 @@ import random
 import re
 import sys
 
+
 class SinglyLinkedListNode:
     def __init__(self, node_data):
         self.data = node_data
         self.next = None
+
 
 class SinglyLinkedList:
     def __init__(self):
@@ -24,15 +26,13 @@ class SinglyLinkedList:
         else:
             self.tail.next = node
 
-
         self.tail = node
+
 
 def print_singly_linked_list(node, sep, fptr):
     while node:
         fptr.write(str(node.data))
-
         node = node.next
-
         if node:
             fptr.write(sep)
 
@@ -57,20 +57,16 @@ def deleteNode(head, positionNodeToDelete):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     llist_count = int(input())
-
     llist = SinglyLinkedList()
-
     for _ in range(llist_count):
         llist_item = int(input())
         llist.insert_node(llist_item)
 
     position = int(input())
-
     llist1 = deleteNode(llist.head, position)
-
     print_singly_linked_list(llist1, ' ', fptr)
     fptr.write('\n')
 
     fptr.close()
+
