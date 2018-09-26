fb345b5c0c41ed3a994781677e27f8cca3fc5a02
zseen <43151120+zseen@users.noreply.github.com>
2018-09-26 21:48:04 +0100

    Refactor iteration:
    - Use a new variable to finish iteration if lists are not the same length

--- Python/LinkedList/MergeTwoSortedLinkedLists/MergeTwoSortedLinkedLists.py
+++ Python/LinkedList/MergeTwoSortedLinkedLists/MergeTwoSortedLinkedLists.py
@@ -60,16 +60,11 @@ def mergeLists(head1, head2):
             head2 = head2.next
         currentNode = currentNode.next
 
-    if head1 is None:
-        while head2 is not None:
-            currentNode.next = head2
-            currentNode = currentNode.next
-            head2 = head2.next
-    elif head2 is None:
-        while head1 is not None:
-            currentNode.next = head1
-            currentNode = currentNode.next
-            head1 = head1.next
+    remainingHead = head1 if head1 is not None else head2
+    while remainingHead is not None:
+        currentNode.next = remainingHead
+        currentNode = currentNode.next
+        remainingHead = remainingHead.next
 
     return head
 
