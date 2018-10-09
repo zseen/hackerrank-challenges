70275ee59e9cf19f3fde468082e43e245fc7bc47
zseen <43151120+zseen@users.noreply.github.com>
2018-10-09 15:20:54 +0100

    Zip the path of the two nodes to find the lowest common ancestor:
    - If the two nodes in the paths are different, the previous node before they diverge is the LCA
    - If one path is shorter than the other (but the nodes in it are the same), the last node of the shorter path is the LCA

--- Python/Tree/BinarySearchTreeLowestCommonAncestor/BinarySearchTreeLowestCommonAncestor.py
+++ Python/Tree/BinarySearchTreeLowestCommonAncestor/BinarySearchTreeLowestCommonAncestor.py
@@ -1,21 +1,22 @@
 from Library.Tree import BinarySearchTree
+from collections import deque
 
 
 def findPath(root, node, path):
     if root:
         if root.value == node:
-            path.append(root)
+            path.appendleft(root)
         else:
             if findPath(root.left, node, path):
-                path.append(root)
+                path.appendleft(root)
             elif findPath(root.right, node, path):
-                path.append(root)
+                path.appendleft(root)
     return path
 
 
 def orderPathFromRootToNode(root, node):
-    route = []
-    pathOrder = list(reversed(findPath(root, node, route)))
+    route = deque()
+    pathOrder = findPath(root, node, route)
     return pathOrder
 
 
@@ -24,11 +25,12 @@ def lca(root, v1, v2):
         firstNodePath = orderPathFromRootToNode(root, v1)
         secondNodePath = orderPathFromRootToNode(root, v2)
 
-        for index in range(0, min(len(firstNodePath), len(secondNodePath))):
-            if firstNodePath[index] != secondNodePath[index]:
-                return firstNodePath[index-1]
+        lowCommAnc = None
+        for value1, value2 in zip(firstNodePath, secondNodePath):
+            if value1 == value2:
+                lowCommAnc = value1
 
-        return firstNodePath[-1] if len(firstNodePath) < len(secondNodePath) else secondNodePath[-1]
+        return lowCommAnc
 
 
 tree = BinarySearchTree.BinarySearchTree()
@@ -36,6 +38,6 @@ nodesList = list((7, 5, 1, 3, 2, 6, 8, 4, 9))
 
 for i in range(0, len(nodesList)):
     tree.insert(nodesList[i])
-lowestCommonAncestor = lca(tree.root, 5, 2)
+lowestCommonAncestor = lca(tree.root, 3, 4)
 print(lowestCommonAncestor.value)
 
