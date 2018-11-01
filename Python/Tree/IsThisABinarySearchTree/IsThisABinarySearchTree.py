0655fbc17d623478ae459b4dfb00276527d391bc
zseen <43151120+zseen@users.noreply.github.com>
2018-11-01 17:48:45 +0000

    Add this exercise - check if a binary tree is a BST

---
+++ Python/Tree/IsThisABinarySearchTree/IsThisABinarySearchTree.py
@@ -0,0 +1,37 @@
+from Library.Tree import BinarySearchTree
+import sys
+
+MIN_INT = - sys.maxsize - 1
+MAX_INT = sys.maxsize
+
+
+def checkIfBST(node, smallestNum, biggestNum):
+    if node is None:
+        return True
+
+    if node.value <= smallestNum or node.value >= biggestNum:
+        return False
+
+    return (checkIfBST(node.left, smallestNum, node.value) and
+            checkIfBST(node.right, node.value, biggestNum))
+
+
+def returnCheckIfBST(node):
+    return checkIfBST(node, MIN_INT, MAX_INT)
+
+
+def main():
+    nodesList = [4, 2, 3, 1, 5, 6, 7]
+
+    tree = BinarySearchTree.BinarySearchTree()
+    for node in range(len(nodesList)):
+        tree.insert(nodesList[node])
+
+    if checkIfBST(tree.root, MIN_INT, MAX_INT):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
