1732a60e2f2795de099aacd30b1ef5856093e119
zseen <43151120+zseen@users.noreply.github.com>
2018-11-03 17:01:23 +0000

    Refactor - construct a tree locally

--- Python/Tree/IsThisABinarySearchTree/IsThisA_BST_InorderTraversal.py
+++ Python/Tree/IsThisABinarySearchTree/IsThisA_BST_InorderTraversal.py
@@ -1,34 +1,21 @@
-
-
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-class Tree:
-    def __init__(self):
-        self.root = None
 
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
-        else:
-            self.insert(value)
-
-
-
-def check(root, visitedNodesValues):
+def checkTreeWithInorderTraversal(root, visitedNodesValues):
     if root:
-        check(root.left, visitedNodesValues)
+        checkTreeWithInorderTraversal(root.left, visitedNodesValues)
         visitedNodesValues.append(root.value)
-        check(root.right, visitedNodesValues)
+        checkTreeWithInorderTraversal(root.right, visitedNodesValues)
 
 
 def getCheckIfBSTResult(root):
     inorderNodeValues = []
 
-    check(root, inorderNodeValues)
+    checkTreeWithInorderTraversal(root, inorderNodeValues)
 
     for i in range(0, len(inorderNodeValues) - 1):
         if inorderNodeValues[i] > inorderNodeValues[i + 1]:
@@ -37,22 +24,13 @@ def getCheckIfBSTResult(root):
 
 
 def main():
-    #nodesList = [4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 14, 16, 16]
-
-    tree = Tree()
-
     root = Node(10)
-    root.right = Node(15)
+    root.right = Node(1)
 
     root.left = Node(2)
 
-    #for node in range(len(nodesList)):
-        #tree.insert(nodesList[node])
-
     isBST = getCheckIfBSTResult(root)
     print("Yes" if isBST else "No")
-
-
 
 if __name__ == "__main__":
     main()
