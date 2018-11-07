d9cebfea694f61f696db0d4a3eec2d277dfe627b
zseen <43151120+zseen@users.noreply.github.com>
2018-11-07 17:13:07 +0000

    Add this exercise

--- Python/Stacks/QueueUsingTwoStacks/QueueUsingTwoStacks.py
+++ Python/Stacks/QueueUsingTwoStacks/QueueUsingTwoStacks.py
@@ -5,16 +5,39 @@ class QueueWithTwoStacks:
     def __init__(self):
         self.appendingStack = []
         self.poppingStack = []
-        self.whatToAppend = None
 
+    def enqueue(self, item):
+        self.appendingStack.append(item)
 
-    def mak
+    def moveItemsFromAppendingToPoppingStackIfEmpty(self):
+        if not self.poppingStack:
+            while self.appendingStack:
+                self.poppingStack.append(self.appendingStack.pop())
+        return self.poppingStack
 
-    def enqueue_appendToRear(self):
-        self.appendingStack.append(self.whatToAppend)
-
-    def dequeue_popFromFront(self):
-        self.poppingStack.pop(0)
+    def dequeue(self):
+        self.moveItemsFromAppendingToPoppingStackIfEmpty()
+        self.poppingStack.pop()
 
     def printFirstItemInQueue(self):
-        
+        self.moveItemsFromAppendingToPoppingStackIfEmpty()
+        print(self.poppingStack[-1])
+
+
+def main():
+    sys.stdin = open("QueueUsingTwoStacks_input.txt")
+    queue = QueueWithTwoStacks()
+
+    n = int(input())
+    for _ in range(n):
+        command = list(map(int, (input().split(' '))))
+        if command[0] == 1:
+            queue.enqueue(command[1])
+        elif command[0] == 2:
+            queue.dequeue()
+        else:
+            queue.printFirstItemInQueue()
+
+
+if __name__ == '__main__':
+    main()
