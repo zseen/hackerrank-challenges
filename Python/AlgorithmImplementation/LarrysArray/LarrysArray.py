7c7a1a1f4be34d97a345a59e86b0fc31f0afe053
zseen <43151120+zseen@users.noreply.github.com>
2019-01-22 15:35:49 +0000

    Use the "inversion counting" method to complete exercise "LarrysArray"

--- Python/AlgorithmImplementation/LarrysArray/LarrysArray.py
+++ Python/AlgorithmImplementation/LarrysArray/LarrysArray.py
@@ -6,9 +6,18 @@ import random
 import re
 import sys
 
-# Complete the larrysArray function below.
+
 def larrysArray(numsArray):
-    
+    inversionCounter = 0
+    for i in range(0, len(numsArray)):
+        for j in range(i + 1, len(numsArray)):
+            if numsArray[i] > numsArray[j]:
+                inversionCounter += 1
+
+    if inversionCounter % 2 == 0:
+        return True
+
+    return False
 
 
 def main():
@@ -17,7 +26,6 @@ def main():
 
     for t_itr in range(t):
         n = int(input())
-
         A = list(map(int, input().rstrip().split()))
 
         result = larrysArray(A)
