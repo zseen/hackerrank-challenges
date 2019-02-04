d5868f0528a28671442661276ba0deccc19a3596
zseen <43151120+zseen@users.noreply.github.com>
2019-02-04 17:38:56 +0000

    Add a working version of exercise "KruskalMST"

---
+++ Python/GraphTheory/KruskalMST_ReallySpecialSubtree/KruskalMST_ReallySpecialSubtree.py
@@ -0,0 +1,69 @@
+#KruskalMST_ReallySpecialSubtree.py
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.parent = None
+
+
+class UnionFind:
+    def __init__(self, numSets):
+        self.nodeIdToNode = {}
+        for i in range(1, numSets + 1):
+            self.nodeIdToNode[i] = Node(i)
+
+    def union(self, id1, id2):
+        p1 = self.find(id1)
+        p2 = self.find(id2)
+
+        p1.parent = p2
+
+    def find(self, id):
+        node = self.nodeIdToNode[id]
+
+        potentialParent = node
+
+        while potentialParent.parent is not None:
+            potentialParent = potentialParent.parent
+
+        return potentialParent
+
+
+
+if __name__ == '__main__':
+    sys.stdin = open("KruskalMST_ReallySpecialSubtree_input.txt")
+    g_nodes, g_edges = map(int, input().split())
+
+    g_from = [0] * g_edges
+    g_to = [0] * g_edges
+    g_weight = [0] * g_edges
+
+    mst = UnionFind(g_nodes)
+
+    graph = []
+    for i in range(g_edges):
+        g_from[i], g_to[i], g_weight[i] = map(int, input().split())
+        graph.append([g_from[i], g_to[i], g_weight[i]])
+
+    graph.sort(key=lambda x:x[2])
+
+    minimumCost = 0
+    for edge in graph:
+        vertex1, vertex2, weight = edge
+        parent1 = mst.find(vertex1)
+        parent2 = mst.find(vertex2)
+        if parent1 != parent2:
+            minimumCost += edge[2]
+            mst.union(vertex1,vertex2)
+
+    print(minimumCost)
+
+
