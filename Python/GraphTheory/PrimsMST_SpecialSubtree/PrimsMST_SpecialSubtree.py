a0faf431923db55fe5639b72c2e6e3a17229d093
zseen <43151120+zseen@users.noreply.github.com>
2019-02-06 20:57:34 +0000

    Work on exercise "PrimsMST"

--- Python/GraphTheory/PrimsMST_SpecialSubtree/PrimsMST_SpecialSubtree.py
+++ Python/GraphTheory/PrimsMST_SpecialSubtree/PrimsMST_SpecialSubtree.py
@@ -11,6 +11,43 @@ from heapq import heappop, heappush
 def prims(nodesNum, edgesWithWeightList, startNode):
     pass
 
+
+
+class Edges:
+    def __init__(self, startVertex, endVertex, weight):
+        self.startVertex = startVertex
+        self.endVertex = endVertex
+        self.weight = weight
+
+
+def getEdgeWithMinimumWeight(graph, node):
+    minWeight = sys.maxsize
+    minWeightEdge = None
+
+    for edge in graph:
+        if edge.startVertex == node:
+            if edge.weight < minWeight:
+                minWeight = edge.weight
+                minWeightEdge = edge
+
+    return minWeightEdge
+
+
+
+def getMST(nodesNum, graph, startNode):
+    visitedNodesSet = set(startNode)
+    minimumWeight = 0
+
+    while len(visitedNodesSet) != nodesNum:
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     sys.stdin = open("PrimsMST_SpecialSubtree_input.txt")
 
@@ -18,9 +55,16 @@ if __name__ == '__main__':
     nodesNum = int(nm[0])
     edgesNum = int(nm[1])
 
+    graph = []
     edges = []
     for _ in range(edgesNum):
         edges.append(list(map(int, input().rstrip().split())))
+
+    for edge in edges:
+        e = Edges(edge[0], edge[1], edge[2])
+        re = Edges(edge[1], edge[0], edge[2])
+        graph.append(e)
+        graph.append(re)
 
     startNode = int(input())
 
