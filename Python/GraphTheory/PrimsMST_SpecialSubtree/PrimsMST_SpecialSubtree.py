0d2d0204000e2229f4563abbd6a4c4f44d9901f3
zseen <43151120+zseen@users.noreply.github.com>
2019-02-10 23:40:22 +0000

    Continue to work on exercise "PrimsMST"

--- Python/GraphTheory/PrimsMST_SpecialSubtree/PrimsMST_SpecialSubtree.py
+++ Python/GraphTheory/PrimsMST_SpecialSubtree/PrimsMST_SpecialSubtree.py
@@ -12,36 +12,51 @@ def prims(nodesNum, graph, startNode):
     return getMST(nodesNum, graph, startNode )
 
 
-
-
-
 class Edge:
-    def __init__(self, nodes, weight):
-        self.nodes = nodes
+    def __init__(self, startVertex, endVertex, weight):
+        self.startVertex = startVertex
+        self.endVertex = endVertex
         self.weight = weight
 
 
+def getMinimumWeightFromNodeEdge(node, edge, visitedNodesList, minValue):
+
+    return minValue
+
+
 def getMST(nodesNum, graph, startNode):
-    visitedNodesSet = set()
-    visitedNodesSet.add(startNode)
-    minValue = sys.maxsize
-    lc = 0
+    visitedNodesList = list()
+    visitedNodesList.append(startNode)
+    minimumWeightTotal = 0
+    bestEdge = None
 
-    while len(visitedNodesSet) != nodesNum:
-        for edge in graph:
-            weight = minValue
-            for node in edge.nodes:
-                if node not in visitedNodesSet and edge.weight < weight:
-                    visitedNodesSet.add(node)
-                    lc += edge.weight
-                    weight = edge.weight
+    while len(set(visitedNodesList)) != nodesNum:
+
+        for node in visitedNodesList:
+            minValue = sys.maxsize
+            for edge in graph:
+                currStart = edge.startVertex
+                currEnd = edge.endVertex
+                currWeight = edge.weight
+                if (node == currStart or node == currEnd) and (
+                                currStart not in visitedNodesList or currEnd not in visitedNodesList) and edge.weight < minValue:
+                    minValue = edge.weight
+                    bestEdge = edge
+            minimumWeightTotal += bestEdge.weight
+            if node == bestEdge.endVertex:
+                visitedNodesList.append(bestEdge.startVertex)
+            else:
+                visitedNodesList.append(bestEdge.endVertex)
 
 
 
 
-    return lc
 
-    #return sum(list(visitedNodesDict.values()))
+
+
+    return minimumWeightTotal
+
+
 
 
 if __name__ == '__main__':
@@ -51,24 +66,19 @@ if __name__ == '__main__':
     nodesNum = int(nm[0])
     edgesNum = int(nm[1])
     edges = []
-    graph = {}
+    #graph = {}
+    graph = []
 
     for _ in range(edgesNum):
         edges.append(list(map(int, input().rstrip().split())))
 
     for edge in edges:
         startVertex, endVertex, weight = edge[0], edge[1], edge[2]
-        e = Edge([startVertex, endVertex], weight)
-        graph[e] = e.weight
-
-
-
-
-
-
-
-
-
+        e = Edge(startVertex, endVertex, weight)
+        # if e.startVertex not in graph:
+        #     graph[e.startVertex] = []
+        # graph[e.startVertex].append(e)
+        graph.append(e)
 
     startNode = int(input())
 
