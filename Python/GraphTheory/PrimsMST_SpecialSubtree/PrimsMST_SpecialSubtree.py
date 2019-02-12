a7a44c54d1dbde7c10f4640409b1eea907b38396
zseen <43151120+zseen@users.noreply.github.com>
2019-02-12 14:19:13 +0000

    Attempt to speed up algorithm - still get timeout on 3 testcases:
    - Graph is a dict
    - Edges are stored twice in graph (key = startVertex, key = endVertex)
    - Edges are removed from graph after using them

--- Python/GraphTheory/PrimsMST_SpecialSubtree/PrimsMST_SpecialSubtree.py
+++ Python/GraphTheory/PrimsMST_SpecialSubtree/PrimsMST_SpecialSubtree.py
@@ -18,22 +18,40 @@ def getMST(nodesNum, graph, startNode):
     visitedNodesList = [startNode]
     minimumWeightTotal = 0
     bestEdge = None
+    potentialEdges = []
+    node = None
 
     while len(set(visitedNodesList)) != nodesNum:
+        potentialEdges = []
         minValue = sys.maxsize
-        for edge in graph:
+        for node in visitedNodesList:
+            for key, value in graph.items():
+                if node == key:
+                    for x in value:
+                        potentialEdges.append(x)
+            #print(potentialEdges)
+
+        for edge in potentialEdges:
             currStart = edge.startVertex
             currEnd = edge.endVertex
             currWeight = edge.weight
             isOneNodeNotVisited = currStart not in visitedNodesList or currEnd not in visitedNodesList
             isOneNodeVisited = currStart in visitedNodesList or currEnd in visitedNodesList
             if isOneNodeNotVisited and isOneNodeVisited and currWeight < minValue:
-                minValue = edge.weight
+                minValue = currWeight
                 bestEdge = edge
         minimumWeightTotal += bestEdge.weight
 
-        visitedNodesList.append(bestEdge.startVertex)
-        visitedNodesList.append(bestEdge.endVertex)
+
+        if bestEdge.startVertex not in visitedNodesList:
+            visitedNodesList.append(bestEdge.startVertex)
+        if bestEdge.endVertex not in visitedNodesList:
+            visitedNodesList.append(bestEdge.endVertex)
+
+
+        graph[bestEdge.startVertex].remove(bestEdge)
+        graph[bestEdge.endVertex].remove(bestEdge)
+
 
     return minimumWeightTotal
 
@@ -45,8 +63,8 @@ if __name__ == '__main__':
     nodesNum = int(nm[0])
     edgesNum = int(nm[1])
     edges = []
-    #graph = {}
-    graph = []
+    graph = {}
+    #graph = []
 
     for _ in range(edgesNum):
         edges.append(list(map(int, input().rstrip().split())))
@@ -54,12 +72,15 @@ if __name__ == '__main__':
     for edge in edges:
         startVertex, endVertex, weight = edge[0], edge[1], edge[2]
         e = Edge(startVertex, endVertex, weight)
-        # if e.startVertex not in graph:
-        #     graph[e.startVertex] = []
-        # graph[e.startVertex].append(e)
-        graph.append(e)
+        if e.startVertex not in graph:
+            graph[e.startVertex] = []
+        if e.endVertex not in graph:
+            graph[e.endVertex] = []
+        graph[e.startVertex].append(e)
+        graph[e.endVertex].append(e)
+        #graph.append(e)
 
     startNode = int(input())
-
+    #print(graph)
     result = getMST(nodesNum, graph, startNode)
     print(result)
