3ea715ec7710ba272f9ffb0ae32dc35ba9e59c10
zseen <43151120+zseen@users.noreply.github.com>
2019-02-17 14:48:31 +0000

    Refactor - queries are executed in a class

--- Python/Heap/QHEAP1.py
+++ Python/Heap/QHEAP1.py
@@ -2,30 +2,34 @@ from heapq import heappush
 import sys
 
 
-def executeHeapQueries(query, heap):
-    instruction = query[0]
-    element = None
-    if len(query) == 2:
-        element = query[1]
+class HeapQueriesExecuter:
+    def __init__(self):
+        self.heap = []
 
-    if instruction == 1:
-        heappush(heap, element)
-    elif instruction == 2:
-        heap.remove(element)
-        heap.sort()
-    elif instruction == 3:
-        print(heap[0])
+    def executeHeapQueries(self, query):
+        instruction = query[0]
+        element = None
+        if len(query) == 2:
+            element = query[1]
+
+        if instruction == 1:
+            heappush(self.heap, element)
+        elif instruction == 2:
+            self.heap.remove(element)
+            self.heap.sort()
+        elif instruction == 3:
+            print(self.heap[0])
 
 
 def main():
     sys.stdin = open("QHEAP1_input.txt")
 
-    heap = []
+    heap = HeapQueriesExecuter()
 
     queriesNum = int((input()).strip())
     for _ in range(queriesNum):
         query = list(map(int, input().strip().split(' ')))
-        executeHeapQueries(query, heap)
+        heap.executeHeapQueries(query)
 
 
 if __name__ == '__main__':
