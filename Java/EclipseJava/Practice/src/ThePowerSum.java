6c7cf43e51ea36be9e1a916f40f600c1027b3167
zseen <43151120+zseen@users.noreply.github.com>
2019-04-10 19:30:42 +0100

    Fix indentation

--- Java/EclipseJava/Practice/src/ThePowerSum.java
+++ Java/EclipseJava/Practice/src/ThePowerSum.java
@@ -7,49 +7,48 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 
-public class ThePowerSum {
-
-    static int powerSum(int total, int power)
+public class ThePowerSum 
+{
+	static int powerSum(int total, int power)
 	{
 		return powerSumRecursive(total, power, 1);
 	}
 
-    static int powerSumRecursive(int total, int power, int base) 
-     {
-        int baseToPower = (int)Math.pow(base, power);
+	static int powerSumRecursive(int total, int power, int base)
+	{
+		int baseToPower = (int)Math.pow(base, power);
 
-        if (baseToPower == total)
-        {
-            return 1;
-        }    
-        else if (baseToPower > total)
-        {
-            return 0;
-        }
-        else
-        {
-            return powerSumRecursive(total, power, base + 1) + powerSumRecursive(total - baseToPower, power, base + 1);
-        }   
-     }
+		if (baseToPower == total)
+		{
+			return 1;
+		}
+			else if (baseToPower > total)
+		{
+			return 0;
+		}
+			else
+		{
+			return powerSumRecursive(total, power, base + 1) + powerSumRecursive(total - baseToPower, power, base + 1);
+		}
+	}
 
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+	public static void main(String[] args) throws IOException
+	{
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int X = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int X = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int N = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int result = powerSum(X, N);
+		int result = powerSum(X, N);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+		bufferedWriter.close();
+		scanner.close();
+	}
 }
