6d05dcd8563e1f5b0ef4e32e8a3ac9b1ca1a3bab
zseen <43151120+zseen@users.noreply.github.com>
2019-04-15 15:42:22 +0100

    Use only one array to store the string numbers

--- Java/EclipseJava/Practice/TheTimeInWords/src/TheTimeInWords.java
+++ Java/EclipseJava/Practice/TheTimeInWords/src/TheTimeInWords.java
@@ -6,13 +6,12 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class TheTimeInWords {
-
-    static final String[] singleDigitNumsInWordFormat = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    static final String[] twoDigitsNumsUpToIncNineteenInWordFormat = new String[]{"ten", "eleven", "twelve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen"};
+public class TheTimeInWords
+{
+    static final String[] numsInWordFormatUpToIncTwenty = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
 
     static String getTimeInWords(int hour, int min)
-    {
+     {
         if (hour > 12 || min > 59)
         {
             throw new IllegalArgumentException("Hour should be <= 12 and minute should be <= 59");
@@ -57,52 +56,31 @@ public class TheTimeInWords {
 
     private static String getHourString(int hour)
     {
-        String hourInWord;
-
-        if (hour <= 9)
-        {
-            hourInWord = singleDigitNumsInWordFormat[hour - 1]; // Correct the index, e.g., 3 is at index 2 in array sinlgeDigits
-        }
-        else
-        {
-            hourInWord = twoDigitsNumsUpToIncNineteenInWordFormat[hour % 10];
-        }
-
-        return hourInWord;
+        return numsInWordFormatUpToIncTwenty[hour - 1]; // Correct the index, e.g., 3 is at index 2 in array sinlgeDigits
     }
 
     private static String getMinutesString(int min)
     {
-        if (min <= 9)
+        if (min <= 20)
         {
-            return singleDigitNumsInWordFormat[min - 1];
+            return numsInWordFormatUpToIncTwenty[min - 1];
         }
-
-        if (min < 20)
-        {
-            return twoDigitsNumsUpToIncNineteenInWordFormat[min % 10];
-        }
-
-        String minuteWord;
 
         if (min < 30)
         {
             int secondDigitInt = min % 20;
-            String secondDigitString = singleDigitNumsInWordFormat[secondDigitInt - 1];
-            minuteWord = "twenty " + secondDigitString;
+            String secondDigitString = numsInWordFormatUpToIncTwenty[secondDigitInt - 1];
+            return "twenty " + secondDigitString;
         }
         else
         {
-            minuteWord = "half"; // If min is 30, the expression will be "half (past)"
+            return "half"; // If min is 30, the expression will be "half (past)"
         }
-
-        return minuteWord;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int h = scanner.nextInt();
@@ -115,6 +93,7 @@ public class TheTimeInWords {
 
         bufferedWriter.write(result);
         bufferedWriter.newLine();
+
         bufferedWriter.close();
 
         scanner.close();
